6d1a1076ce15091043c706203ea43f6563ea94ea
othniel2471 <othnielomaidu@gmail.com>
2023-08-15 15:26:37 +0100

    used querries and updated the animals table

--- data.sql
+++ data.sql
@@ -6,3 +6,6 @@ INSERT INTO animals (NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG)
 VALUES ('Gabumon', '2018-11-15', 2, true, 8.0);
 INSERT INTO animals (NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG)
 VALUES('Pikachu', '2021-01-07', 1, false, 15.04),('Devimon', '2017-05-12', 5, true, 11.0);
+VALUES ('Charmander', '2020-02-08', 0, false, -11.0);
+INSERT INTO animals (NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG)
+VALUES ('Plantmon', '2021-11-15', 2, true, -5.7), ('Squirtle', '1993-04-02', 3, false, -12.13), ('Angemon', '2005-06-12', 1, true, -45.0), ('Boarmon', '2005-06-07', 7, true, 20.4), ('Blossom', '1998-10-13', 3, true, 17.0), ('Ditto', '2022-05-14', 4, true, 22.0);
